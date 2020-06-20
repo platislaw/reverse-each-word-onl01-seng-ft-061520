9cc31db152bac546b218010abca8e3bbe9083669
Paul Latislaw <paullatislaw@gmail.com>
2020-06-20 03:21:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(phrase)
- reverse_phrase = []
-  phrase.each do |reverse_it|
-    reverse_phrase << phrase.reverse 
-end
-reverse_phrase
-end
+# def reverse_each_word(phrase)
+#  reverse_phrase = []
+#   phrase.each do |reverse_it|
+#     reverse_phrase << phrase.reverse
+# end
+# reverse_phrase
+# end
 
  # "olleH ,ereht dna woh era ?uoy"
